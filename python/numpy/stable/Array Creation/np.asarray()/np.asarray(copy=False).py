76caded3068af4d3163b37321460781db64bded2
Itz-MrJ <arya1649481641589@gmail.com>
2024-11-14 00:41:20 +0530

    np.asarray()

---
+++ python/numpy/stable/Array Creation/np.asarray()/np.asarray(copy=False).py
@@ -0,0 +1,14 @@
+import numpy as np
+
+# Original array
+original_array = np.asarray([1, 2, 3])
+
+# Creating a new array with copy=False (view)
+new_array = np.asarray(original_array, copy=False)
+
+# Modify the new array
+new_array[0] = 99
+
+# Print both arrays
+print("Original Array:", original_array)  # Output: [99 2 3]
+print("New Array:", new_array)            # Output: [99 2 3]
---
+++ python/numpy/stable/Array Creation/np.zeros()/np.zeros(shape=).py
@@ -0,0 +1,9 @@
+import numpy as np
+# (row, col) or [row, col] to be fed as shape
+zeros_array = np.zeros([2, 3])
+print(zeros_array)
+'''
+Output: 
+[[0. 0. 0.]
+ [0. 0. 0.]]
+'''
