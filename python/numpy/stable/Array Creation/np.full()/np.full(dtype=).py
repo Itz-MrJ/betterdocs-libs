cd40650b8f65d8a424cc91504305384447218a38
Itz-MrJ <arya1649481641589@gmail.com>
2024-11-20 13:31:53 +0530

    fixes

---
+++ python/numpy/stable/Array Creation/np.full()/np.full(dtype=).py
@@ -0,0 +1,9 @@
+import numpy as np
+
+# 2x2 array filled with 4
+full_array = np.full((2, 2), 4, dtype=np.float64)
+print(full_array)
+'''
+[[4. 4.]
+ [4. 4.]]
+'''
--- python/numpy/stable/Array Creation/np.zeros()/np.zeros(shape=).py
+++ python/numpy/stable/Array Creation/np.zeros()/np.zeros(shape=).py
@@ -1,9 +1,9 @@
 import numpy as np
 # (row, col) or [row, col] to be fed as shape
-zeros_array = np.zeros([2, 3])
+zeros_array = np.zeros(shape=[2, 3])
 print(zeros_array)
 '''
-Output: 
+Output:
 [[0. 0. 0.]
  [0. 0. 0.]]
 '''
